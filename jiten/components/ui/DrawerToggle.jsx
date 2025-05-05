1c49a4d667ea66a0c33f4953f1f18f2ab84813f8
Jinxy2004 <jinxy2006@yahoo.com>
2025-05-04 23:59:26 -0400

    Formatting

--- jiten/components/ui/DrawerToggle.jsx
+++ jiten/components/ui/DrawerToggle.jsx
@@ -1,20 +1,23 @@
-// components/DrawerToggle.tsx
-import { useNavigation } from 'expo-router';
-import { DrawerActions } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { useTheme } from '../ThemeContext';
+import { useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "../ThemeContext";
 
 export function DrawerToggle() {
   const navigation = useNavigation();
-  const {theme} = useTheme();
+  const { theme } = useTheme();
 
   return (
     <TouchableOpacity
       onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
       style={{ paddingHorizontal: 4 }}
     >
-      <Ionicons name="menu" size={24} color={theme ==="dark" ? '#fff' : "#000"} />
+      <Ionicons
+        name="menu"
+        size={24}
+        color={theme === "dark" ? "#fff" : "#000"}
+      />
     </TouchableOpacity>
   );
 }
--- jiten/components/ui/SettingsToggle.jsx
+++ jiten/components/ui/SettingsToggle.jsx
@@ -1,19 +1,22 @@
-// components/DrawerToggle.tsx
-import { TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { useTheme } from '../ThemeContext';
-import { useRouter } from 'expo-router';
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "../ThemeContext";
+import { useRouter } from "expo-router";
 
 export function SettingsToggle() {
   const router = useRouter();
-  const {theme} = useTheme();
+  const { theme } = useTheme();
 
   return (
     <TouchableOpacity
       onPress={() => router.push("/settings")}
       style={{ paddingHorizontal: 4 }}
     >
-      <Ionicons name="settings-outline" size={24} color={theme ==="dark" ? '#fff' : "#000"} />
+      <Ionicons
+        name="settings-outline"
+        size={24}
+        color={theme === "dark" ? "#fff" : "#000"}
+      />
     </TouchableOpacity>
   );
 }
